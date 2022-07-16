7d83a688cb06a2630b26bd7713226734821a9ddd
sajibAdhi <sajibadhikery@gmail.com>
2022-07-16 23:32:25 +0600

    37. Passing Data via "props"

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    { id: 'e0', title: 'Car Invoice', amount: 100.64, date: new Date(2020, 2, 1) },
+    { id: 'e1', title: 'Rent', amount: 500.64, date: new Date(2020, 2, 1) },
+    { id: 'e2', title: 'Food', amount: 100.64, date: new Date(2020, 2, 1) },
+    { id: 'e3', title: 'Truck', amount: 100.64, date: new Date(2020, 2, 1) },
+    { id: 'e4', title: 'Pizza', amount: 500.64, date: new Date(2020, 2, 1) },
+    { id: 'e5', title: 'Burger', amount: 100.64, date: new Date(2020, 2, 1) },
+  ];
+
   return (
     <div className="App">
       {/* <header className="App-header">
@@ -20,10 +29,12 @@ function App() {
         </a>
       </header> */}
 
-      <ExpenseItem></ExpenseItem>
+      {expenses.map(expence =>
+        <ExpenseItem date={expence.date} title={expence.title} amount={expence.amount}></ExpenseItem>
+      )}
 
     </div>
-    
+
   );
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenceDate = new Date(2020, 2, 1);
-    const expenceTitle = 'Car Invoice';
-    const expenceAmount = 100.64;
+function ExpenseItem(props) {
+    // const expenceDate = new Date(2020, 2, 1);
+    // const expenceTitle = 'Car Invoice';
+    // const expenceAmount = 100.64;
 
     return (
         <div className='expense-item'>
-            <div>{expenceDate.toISOString}</div>
+            <div>{props.date.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>{expenceTitle}</h2>
-                <div className='expense-item__price'>${expenceAmount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
         </div>
     );
